07a832686efe750e823d7d9e3609ca0ae362e8da
Patrick Leee <patrickleee@foxmail.com>
2018-06-11 18:32:46 +0800

    jenkins maven打包报错

--- src/main/java/com/demo/model/dto/CustomUserDetails.java
+++ src/main/java/com/demo/model/dto/CustomUserDetails.java
@@ -5,14 +5,13 @@ import com.google.common.collect.Sets;
 import lombok.Getter;
 import lombok.ToString;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.collection.UnmodifiableCollection;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Desciption
@@ -44,12 +43,12 @@ public class CustomUserDetails extends User implements UserDetails {
             this.accountNonExpired = accountNonExpired;
             this.credentialsNonExpired = credentialsNonExpired;
             this.accountNonLocked = accountNonLocked;
-//            Set<GrantedAuthority> set = Sets.newHashSet();
+//            Set<GrantedAuthority> set = Collections.unmodifiableSet(Sets.newHashSet());
+//            this.authorities = Collections.unmodifiableSet(set);
 //            if (authorities != null || authorities.size() > 0) {
 //                set.addAll(authorities);
 //            }
-//            this.authorities = Collections.unmodifiableSet(set);
-            this.authorities = Collections.unmodifiableSet(new HashSet<>(CollectionUtils.emptyIfNull(authorities)));
+            this.authorities = Collections.unmodifiableSet((CollectionUtils.emptyIfNull(authorities)).stream().collect(Collectors.toSet()));
         } else {
             throw new IllegalArgumentException("Cannot pass null or empty values to constructor");
         }
